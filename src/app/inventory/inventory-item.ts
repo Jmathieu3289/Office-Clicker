ce7c98140c77de587914624a27800219eec45804
Sendiks <Justin.Mathieu@sendiksmarket.com>
2017-10-14 01:57:18 -0500

    Making windows move around

--- src/app/inventory/inventory-item.ts
+++ src/app/inventory/inventory-item.ts
@@ -1,3 +1,5 @@
+import { Rarity } from '../rarity.enum';
+
 export class InventoryItem {
 
   static PREFIXES = ['Speedy', 'Sharp', 'Whacky'];
@@ -7,22 +9,26 @@ export class InventoryItem {
   type: string;
   img_id: string;
   uuid: any;
+  rarities: Array<Rarity> = [Rarity.ARTIFACT, Rarity.COMMON, Rarity.EPIC, Rarity.LEGENDARY, Rarity.RARE, Rarity.UNCOMMON];
+  rarity: Rarity = Rarity.ARTIFACT;
 
   constructor(name: string, type: string) {
-    this.name = InventoryItem.PREFIXES[Math.floor(Math.random()*InventoryItem.PREFIXES.length)] + " " + name + " " + InventoryItem.SUFFIXES[Math.floor(Math.random()*InventoryItem.SUFFIXES.length)];
+    this.rarity = this.rarities[Math.floor(Math.random() * this.rarities.length)];
+    this.name = InventoryItem.PREFIXES[Math.floor(Math.random() * InventoryItem.PREFIXES.length)] +
+    ' ' + name + ' ' + InventoryItem.SUFFIXES[Math.floor(Math.random() * InventoryItem.SUFFIXES.length)];
     this.type = type;
     this.uuid = this.uuidv4();
-    if(this.type=='head'){
-     this.img_id = '/assets/Helmet/CommandButtons/BTNINV_Helmet_0'+Math.ceil(Math.random()*9)+'.jpg';
-    }else if(this.type=='body'){
-     this.img_id = '/assets/Chest/CommandButtons/BTNINV_Chest_Chain_0'+Math.ceil(Math.random()*9)+'.jpg';
-    }else{
-     this.img_id = '/assets/Weapon/CommandButtons/BTNINV_Sword_0'+Math.ceil(Math.random()*9)+'.jpg'; 
+    if (this.type === 'head') {
+     this.img_id = '/assets/Helmet/CommandButtons/BTNINV_Helmet_0' + Math.ceil(Math.random() * 9) + '.jpg';
+    }else if (this.type === 'body') {
+     this.img_id = '/assets/Chest/CommandButtons/BTNINV_Chest_Chain_0' + Math.ceil(Math.random() * 9) + '.jpg';
+    }else {
+     this.img_id = '/assets/Weapon/CommandButtons/BTNINV_Sword_0' + Math.ceil(Math.random() * 9) + '.jpg';
     }
   }
 
   uuidv4() {
-    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c:any) =>
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
+
 
 import { InventoryItem } from './inventory-item';
 
@@ -10,21 +11,77 @@ import { InventoryItem } from './inventory-item';
 })
 export class InventoryComponent implements OnInit {
 
-  size: number = 144;
+  size = 144;
+  state = 'windowed';
+  visible = true;
+  left = 0;
+  top = 0;
+
+  private el: HTMLElement;
+
+  private mouseDown = false;
+  private xOffset = 0;
+  private yOffset = 0;
+
   @Input() inventory: Array<InventoryItem>;
   @Input() slots: Array<InventoryItem>;
 
-  constructor() {
+  @HostListener('window:mouseup')
+  onWindowMouseup() {
+      console.log('mouse up');
+      this.mouseDown = false;
+  }
+
+  @HostListener('mouseup')
+  onMouseup() {
+      this.mouseDown = false;
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  onMousemove(event: MouseEvent) {
+      if (this.mouseDown) {
+        this.left = event.clientX - this.xOffset;
+        this.top = event.clientY - this.yOffset;
+      }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMousedown(event) {
+      if (event.target.tagName === 'H1') {
+        this.mouseDown = true;
+        this.xOffset = event.clientX - this.left;
+        this.yOffset = event.clientY - this.top;
+      }
+  }
+
+  constructor(elementRef: ElementRef) {
+    this.el = elementRef.nativeElement;
   }
 
   ngOnInit() {
   }
 
   onItemDrop(e: any) {
-    if(e.dragData.src != null){
-    	this.slots[e.dragData.src] = null;
-    	e.dragData.src = null;
-    	this.inventory.push(e.dragData);
+    if (e.dragData.src != null) {
+      this.slots[e.dragData.src] = null;
+      e.dragData.src = null;
+      this.inventory.push(e.dragData);
+    }
+  }
+
+  minimize() {
+    this.visible = false;
+  }
+
+  close() {
+    this.visible = false;
+  }
+
+  toggleSize() {
+    if (this.state === 'windowed') {
+      this.state = 'fullscreen';
+    }else {
+      this.state = 'windowed';
     }
   }
 
